2483d195e19ac0dd11ff2484108f3c3ed464259d
qunax <procop4uck.ilia@gmail.com>
2022-06-21 00:14:10 +0200

    created AddNewTrackVM & made buttons in its View

---
+++ MusicDiary/Commands/AddNewTrackCommand.cs
@@ -0,0 +1,51 @@
+﻿using MusicDiary.Services;
+using MusicDiary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicDiary.Commands
+{
+    public class AddNewTrackCommand : CommandBase
+    {
+        private AddTrackViewModel _addTrackViewModel;
+        private readonly NavigationService _mainMenunavigationService;
+
+        public AddNewTrackCommand(AddTrackViewModel addTrackViewModel, NavigationService mainMenuNavigationService)
+        {
+            _addTrackViewModel = addTrackViewModel;
+            _addTrackViewModel.PropertyChanged += OnViewPropertyChanged;
+            _mainMenunavigationService = mainMenuNavigationService;
+        }
+
+        private void OnViewPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(AddTrackViewModel.TrackTitle) ||
+                e.PropertyName == nameof(AddTrackViewModel.TrackGenre) ||
+                e.PropertyName == nameof(AddTrackViewModel.TrackAlbumName) ||
+                e.PropertyName == nameof(AddTrackViewModel.TrackArtistName) ||
+                e.PropertyName == nameof(AddTrackViewModel.TrackTextLink))
+            {
+                OnCanExecuteChanged();
+            }
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return !string.IsNullOrEmpty(_addTrackViewModel.TrackTitle)
+                && !string.IsNullOrEmpty(_addTrackViewModel.TrackGenre)
+                && !string.IsNullOrEmpty(_addTrackViewModel.TrackAlbumName)
+                && !string.IsNullOrEmpty(_addTrackViewModel.TrackArtistName)
+                && !string.IsNullOrEmpty(_addTrackViewModel.TrackTextLink)
+                && base.CanExecute(parameter);
+        }
+
+        public override void Execute(object parameter)
+        {
+            _mainMenunavigationService.Navigate();
+        }
+    }
+}
--- MusicDiary/ViewModels/AddTrackViewModel.cs
+++ MusicDiary/ViewModels/AddTrackViewModel.cs
@@ -9,14 +9,88 @@ using System.Windows.Input;
 
 namespace MusicDiary.ViewModels
 {
-    class AddTrackViewModel : ViewModelBase
+    public class AddTrackViewModel : ViewModelBase
     {
+        private string _trackTitle;
+        public string TrackTitle
+        {
+            get
+            {
+                return _trackTitle;
+            }
+            set
+            {
+                _trackTitle = value;
+                OnPropertyChanged(nameof(TrackTitle));
+            }
+        }
+
+        private string _trackGenre;
+        public string TrackGenre
+        {
+            get
+            {
+                return _trackGenre;
+            }
+            set
+            {
+                _trackGenre = value;
+                OnPropertyChanged(nameof(TrackGenre));
+            }
+        }
+
+        private string _trackAlbumname;
+        public string TrackAlbumName
+        {
+            get
+            {
+                return _trackAlbumname;
+            }
+            set
+            {
+                _trackAlbumname = value;
+                OnPropertyChanged(nameof(TrackAlbumName));
+            }
+        }
+
+        private string _trackArtistName;
+        public string TrackArtistName
+        {
+            get
+            {
+                return _trackArtistName;
+            }
+            set
+            {
+                _trackArtistName = value;
+                OnPropertyChanged(nameof(TrackArtistName));
+            }
+        }
+
+        private string _trackTextLink;
+        public string TrackTextLink
+        {
+            get
+            {
+                return _trackTextLink;
+            }
+            set
+            {
+                _trackTextLink = value;
+                OnPropertyChanged(nameof(TrackTextLink));
+            }
+        }
+
+
+
+        public ICommand AddCover { get; }
         public ICommand CancelCommand { get; }
         public ICommand SubmitCommand { get; }
 
         public AddTrackViewModel(NavigationService mainMenuNavigationService)
         {
             CancelCommand = new NavigateCommand(mainMenuNavigationService);
+            SubmitCommand = new AddNewTrackCommand(this, mainMenuNavigationService);
         }
     }
 }
